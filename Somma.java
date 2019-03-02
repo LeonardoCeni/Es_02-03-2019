26f152329295a5bd09a5313ad1c450e1bb77c0a1
Leonardo Ceni <studente.ceni.leonardo@gmail.com>
2019-03-02 11:24:49 +0100

    Deleted HelloWorld.java, Created Somma.java, Added HelloWorld.cpp

---
+++ Somma.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Somma{
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the value for the variable a: ");
+    int a = input.nextInt();
+    System.out.println("Enter the value for the variable b: ");
+    int b = input.nextInt();
+    System.out.println("Result of a+b = " + a+b);
+}
